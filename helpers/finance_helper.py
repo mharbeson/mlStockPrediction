63f6832f316d4c7e8530e8c31cab6a79fb69b673
Matt Harbeson <matthew.harbeson@gmail.com>
2022-07-25 22:11:02 -0400

    first commit

---
+++ helpers/finance_helper.py
@@ -0,0 +1,80 @@
+import yfinance as yf
+import os
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
+
+
+class Stock:
+    def __init__(self, ticker_id='', data_path='data/yahoo_data'):
+        self.ticker = ticker_id
+        self.path = data_path
+        self.ticker_file = f'{self.path}/{self.ticker}_data.json'
+
+
+    def retrieve_stock_data(self):
+        ''' Return dataframe for ticker and write to file '''
+        if os.path.exists(self.ticker_file):
+            with open(self.ticker_file) as f:
+                self.hist = pd.read_json(self.ticker_file)
+        else:
+            self.ticker = yf.Ticker(self.ticker)
+            self.hist = self.ticker.history(period="max")
+
+            self.hist.to_json(self.ticker_file)
+        
+        return self.hist
+
+    def basic_ticker_info(self):
+        ''' Generate head for dataframe and display plot graph of historical rpice '''
+        self.pd = self.retrieve_stock_data()
+
+        print(self.pd.head(5))
+        self.pd.plot.line(y="Close", use_index=True)
+        # plt.show()
+
+    def prep_training_data(self):
+        self.data = self.retrieve_stock_data()
+        self.shifted_data = self.data.copy()
+        self.shifted_data = self.shifted_data.shift(1)
+        self.data = self.data[['Close']]
+        self.data = self.data.rename(columns= {'Close':'ActualClose'})
+        # print(self.data.head(5))
+        self.data['Target'] = self.data.rolling(2).apply(lambda x: x.iloc[1] > x.iloc[0])['ActualClose']
+
+        # Shift prices forward
+        self.market_predictors = ['Close', 'Volume', 'Open', 'High', 'Low']
+        self.training_data = self.data.join(self.shifted_data[self.market_predictors]).iloc[1:]
+
+        # print(self.training_data.head(10))
+
+        return self.market_predictors, self.training_data
+
+
+class Learning_Model:
+    def __init__(self, predictors, training_data):
+        self.predictors = predictors
+        self.training_data = training_data
+        self.model = RandomForestClassifier(
+            n_estimators=100,
+            min_samples_split=2000,
+            random_state=1
+        )
+        self.train = self.training_data.iloc[:-100]
+        self.test = self.training_data.iloc[-100:]
+
+        self.model.fit(self.train[predictors], self.train["Target"])
+
+    def check_precision_score(self):
+        self.predictors = self.model.predict(self.test[self.predictors])
+        self.predictors = pd.Series(self.predictors, index=self.test.index)
+        self.precision = precision_score(self.test['Target'], self.predictors)
+
+        # Round
+        self.precision = round(self.precision * 100, 2)
+        return self.precision
+
+    # def backetesting(self, training_data):
---
+++ main.py
@@ -0,0 +1,29 @@
+from helpers.finance_helper import Stock
+from helpers.finance_helper import Learning_Model
+import matplotlib.pyplot as plt
+
+def ticker_prompt(ticker=''):
+    if ticker == '':
+        ticker = input('Input stock ticker for analysis: ')
+        return ticker, Stock(ticker)
+    else:
+        return ticker, Stock(ticker)
+
+# MSFT
+# AAPL
+# AMZN
+# GOOG
+ticker_name, stock1 = ticker_prompt('AAPL')
+print(f'Generating results for {ticker_name}')
+# stock1.basic_ticker_info()
+predictors, training_data = stock1.prep_training_data()
+
+# print(training_data.head(5))
+
+stock_model1 = Learning_Model(predictors, training_data)
+print(f'Model is directionally accurate {stock_model1.check_precision_score()}% of the time.')
+
+
+stock_model2 = Learning_Model(predictors, training_data)
+
+print(stock_model1.predictors)
